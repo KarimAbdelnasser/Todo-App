2c8c37009b387a0e11a138b038caa45ed1eb2be5
KarimAbdelnasser <kareemabdo38@yahoo.com>
2023-06-17 07:23:07 +0300

    config

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
