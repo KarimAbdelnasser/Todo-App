f4a99b5b82251056f08b66742bb6308da09aae7b
KarimAbdelnasser <kareemabdo38@yahoo.com>
2023-06-17 04:09:15 +0300

    connect to db first

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const app = require("./app");
 const http = require("http").Server(app);
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 
-http.listen(PORT, () => {
-    console.log(`Running on port ${PORT}...`);
-});
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB...");
+        // Start the server
+        http.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}...`);
+        });
+    })
+    .catch((error) => {
+        console.error("Failed to connect to MongoDB:", error);
+    });
